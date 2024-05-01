08f65fc0fcc664b7bf66024ce448f003e39fa8fc
Simmo Saan <simmo.saan@gmail.com>
2024-05-01 16:42:24 +0300

    Make Bootstrap tables non-bordered and small

--- assets/js/no_defer.js
+++ assets/js/no_defer.js
@@ -13,10 +13,10 @@ $(document).ready(function() {
         $(this).parents('[class*="archive"]').length==0 &&
         $(this).parents('code').length == 0) {
       // make table use bootstrap-table
-      $(this).attr('data-toggle','table');
       // add some classes to make the table look better
-      // $(this).addClass('table-sm');
-      $(this).addClass('table-hover');
+      $(this).bootstrapTable({
+        classes: 'table table-hover table-sm'
+      });
     }
   })
 });
