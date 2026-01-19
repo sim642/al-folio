279a4385aa0d41cf1ff86cb20a484a6103c91edf
Simmo Saan <simmo.saan@gmail.com>
2026-01-19 23:57:08 +0200

    Add margin to zoomed image

--- assets/js/zoom.js
+++ assets/js/zoom.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
   let navbarHeight = $("#navbar").outerHeight(true);
   medium_zoom = mediumZoom("[data-zoomable]", {
     background: getComputedStyle(document.documentElement).getPropertyValue("--global-bg-color") + "ee", // + 'ee' for trasparency.
+    margin: 24,
     container: {
       top: navbarHeight
     }
