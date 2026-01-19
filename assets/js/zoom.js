3b50cda630e1870ae0deacd0314b86b9f49f2917
Simmo Saan <simmo.saan@gmail.com>
2026-01-19 23:56:52 +0200

    Leave navbar space for zoomed image

--- assets/js/zoom.js
+++ assets/js/zoom.js
@@ -1,6 +1,10 @@
 // Initialize medium zoom.
 $(document).ready(function () {
+  let navbarHeight = $("#navbar").outerHeight(true);
   medium_zoom = mediumZoom("[data-zoomable]", {
     background: getComputedStyle(document.documentElement).getPropertyValue("--global-bg-color") + "ee", // + 'ee' for trasparency.
+    container: {
+      top: navbarHeight
+    }
   });
 });
